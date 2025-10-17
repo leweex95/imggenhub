9dbe0b1618976aaf7875a6e77df83f5eca158840
leweex95 <leweex95@users.noreply.github.com>
2025-10-17 15:51:53 +0200

    fixed unit test running bug

--- tests/test_main.py
+++ tests/test_main.py
@@ -9,7 +9,7 @@ class TestMainPipeline(unittest.TestCase):
     @patch('imggenhub.kaggle.main.resolve_prompts', return_value=['prompt'])
     @patch('pathlib.Path.mkdir')
     def test_run_pipeline_success(self, mock_mkdir, mock_resolve, mock_deploy, mock_poll, mock_download):
-        main.run_pipeline(None, 'notebook', 'kernel_path', gpu=True, dest='output_images')
+        main.run_pipeline(None, './config/kaggle-notebook-image-generation.ipynb', './config', gpu=True, dest='output_images')
         mock_deploy.assert_called()
         mock_poll.assert_called()
         mock_download.assert_called()
@@ -21,7 +21,7 @@ class TestMainPipeline(unittest.TestCase):
     @patch('pathlib.Path.mkdir')
     def test_run_pipeline_kernel_error(self, mock_mkdir, mock_resolve, mock_deploy, mock_poll, mock_download):
         with self.assertRaises(RuntimeError):
-            main.run_pipeline(None, 'notebook', 'kernel_path', gpu=True, dest='output_images')
+            main.run_pipeline(None, './config/kaggle-notebook-image-generation.ipynb', './config', gpu=True, dest='output_images')
 
 if __name__ == '__main__':
     unittest.main()
